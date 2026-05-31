d2d9228d05267473a991446a2e05e77fcaa92512
Tom <tom-anders@gmx.net>
2026-05-31 12:26:07 +0200

    Deploy 31.05.2026 12:26:07,80

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,12 @@
 @echo off
 cd /d C:\Users\tom-a\werwolf-app
+
+:: Auto-increment service worker cache version
+for /f "tokens=2 delims=-" %%A in ('findstr /r "werwolf-v[0-9]*" sw.js') do set OLD=%%A
+for /f "tokens=2 delims=v" %%B in ('findstr /r "werwolf-v[0-9]*" sw.js') do set /a NEW=%%B+1
+powershell -Command "(Get-Content sw.js) -replace 'werwolf-v[0-9]+', 'werwolf-v%NEW%' | Set-Content sw.js"
+echo SW-Cache: werwolf-v%NEW%
+
 git add .
 git commit -m "Deploy %date% %time%"
 git push
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'werwolf-v15';
+const CACHE = 'werwolf-v16';
 const ASSETS = [
   './index.html',
   './css/styles.css',
